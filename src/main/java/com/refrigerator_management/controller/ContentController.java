3a6a5526bc4995833403f5a61b179fa488ce3c31
Alan <1406441338@qq.com>
2023-03-07 17:41:43 +0800

    test

--- src/main/java/com/refrigerator_management/controller/ContentController.java
+++ src/main/java/com/refrigerator_management/controller/ContentController.java
@@ -24,6 +24,7 @@ public class ContentController {
      */
     @GetMapping("/content")
     public List<ContentShow> GetContentInfo(int refrigerator_id,int layer_id){
+        System.out.println('y');
         return contentService.GetContent(refrigerator_id,layer_id);
     }
 
