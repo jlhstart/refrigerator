2d7ad9d3a0b2f53c46da2f55f4cb2fcfd5924fd1
Alan <1406441338@qq.com>
2023-03-11 21:38:40 +0800

    2023/3/11 Alan_2

--- src/main/java/com/refrigerator_management/entity/Store.java
+++ src/main/java/com/refrigerator_management/entity/Store.java
@@ -14,10 +14,10 @@ public class Store {
     private Integer layerId ;
     private Double amount ;
     private String unit ;
-    @DateTimeFormat(pattern = "yyy-MM-dd")
-    @JsonFormat(pattern = "yyy-MM-dd")
+/*    @DateTimeFormat(pattern = "yyy-MM-dd")
+    @JsonFormat(pattern = "yyy-MM-dd")*/
     private Date dateStore ;
-    @DateTimeFormat(pattern = "yyy-MM-dd")
-    @JsonFormat(pattern = "yyy-MM-dd")
+/*    @DateTimeFormat(pattern = "yyy-MM-dd")
+    @JsonFormat(pattern = "yyy-MM-dd")*/
     private Date dateExpired;
 }
